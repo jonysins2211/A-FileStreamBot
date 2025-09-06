77b7a317452330b1835ddeea15f380c386487515
jonysins2211 <Jonysins11220@gmail.com>
2025-09-07 04:23:51 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -1,57 +1,36 @@
 import aiohttp
 import jinja2
 import urllib.parse
-import logging
-
 from FileStream.config import Telegram, Server
 from FileStream.utils.database import Database
 from FileStream.utils.human_readable import humanbytes
 
 db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
-
-class InvalidHash(Exception):
-    """Raised when secure hash validation fails."""
-    pass
-
-
-async def render_page(db_id, secure_hash, src=None):
-    # Fetch file data from DB
+async def render_page(db_id, src=None):
+    # Get file details from database
     file_data = await db.get_file(db_id)
+    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
+    file_size = humanbytes(file_data['file_size'])
+    file_name = file_data['file_name'].replace("_", " ")
 
-    # Verify secure hash using unique_id (first 6 chars)
-    if file_data["file_unique_id"][:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data['file_unique_id'][:6]}")
-        logging.debug(f"Invalid hash for DB file with - ID {db_id}")
-        raise InvalidHash
-
-    # Build secure file URL
-    src = urllib.parse.urljoin(
-        Server.URL,
-        f"dl/{file_data['_id']}?hash={secure_hash}"
-    )
-
-    # Human readable file size
-    file_size = humanbytes(file_data["file_size"])
-    file_name = file_data["file_name"].replace("_", " ")
-
-    # Choose template based on file type
-    tag = file_data["mime_type"].split("/")[0].strip()
-    if tag in ["video", "audio"]:
+    # Decide which template to load
+    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
         template_file = "FileStream/template/play.html"
     else:
         template_file = "FileStream/template/dl.html"
+        # If not video, fetch real file size
         async with aiohttp.ClientSession() as s:
             async with s.get(src) as u:
                 file_size = humanbytes(int(u.headers.get("Content-Length")))
 
-    # Render template
-    with open(template_file, encoding="utf-8") as f:
+    # Load template
+    with open(template_file) as f:
         template = jinja2.Template(f.read())
 
+    # Render template
     return template.render(
         file_name=file_name,
         file_url=src,
-        file_size=file_size,
-        file_unique_id=file_data["file_unique_id"],
+        file_size=file_size
     )
