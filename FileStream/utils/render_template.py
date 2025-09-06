e2f6f31102bbc034211a78ed36c9409827876e2f
jonysins2211 <Jonysins11220@gmail.com>
2025-09-07 03:40:25 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -5,27 +5,45 @@ from FileStream.config import Telegram, Server
 from FileStream.utils.database import Database
 from FileStream.utils.human_readable import humanbytes
 
+# Initialize database
 db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
-async def render_page(db_id):
-    file_data = await db.get_file(db_id)
-    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
-    file_size = humanbytes(file_data['file_size'])
-    file_name = file_data['file_name'].replace("_", " ")
+# Jinja2 template loader
+def load_template(path: str):
+    with open(path, encoding="utf-8") as f:
+        return jinja2.Template(f.read())
 
-    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
+async def render_page(db_id: str):
+    """
+    Render a play/download page for the given file ID
+    """
+    file_data = await db.get_file(db_id)
+    if not file_data:
+        return "File not found"
+
+    # File details
+    file_url = urllib.parse.urljoin(Server.URL, f"dl/{file_data['_id']}")
+    file_size = humanbytes(file_data["file_size"])
+    file_name = file_data["file_name"].replace("_", " ")
+    mime_type = file_data.get("mime_type", "")
+
+    # Decide template: play.html for videos, dl.html for others
+    if mime_type.startswith("video"):
         template_file = "FileStream/template/play.html"
     else:
         template_file = "FileStream/template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get('Content-Length')))
+        # If not a video, confirm size via HEAD request
+        async with aiohttp.ClientSession() as session:
+            async with session.head(file_url) as resp:
+                if resp.headers.get("Content-Length"):
+                    file_size = humanbytes(int(resp.headers["Content-Length"]))
 
-    with open(template_file) as f:
-        template = jinja2.Template(f.read())
+    # Load template
+    template = load_template(template_file)
 
+    # Render with values
     return template.render(
         file_name=file_name,
-        file_url=src,
+        file_url=file_url,
         file_size=file_size
     )
